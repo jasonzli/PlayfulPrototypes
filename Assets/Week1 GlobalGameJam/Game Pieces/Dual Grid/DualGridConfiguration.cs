a6947fd7ff67735546dc63fec60b7706600d2fb5
Jason Li <jasonli1337@gmail.com>
2022-01-30 02:45:43 -0500

    Add movement curve to dual grid

--- Assets/Week1 GlobalGameJam/Game Pieces/Dual Grid/DualGridConfiguration.cs
+++ Assets/Week1 GlobalGameJam/Game Pieces/Dual Grid/DualGridConfiguration.cs
@@ -9,5 +9,6 @@ namespace OvenFresh
         public BoardConfiguration xyGridConfig;
         public BoardConfiguration zyGridConfig;
         public MovementMode startingMode;
+        public CurveParameter movementCurve;
     }
 }
