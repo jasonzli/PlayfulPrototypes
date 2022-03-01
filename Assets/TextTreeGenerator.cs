9c46101bd01b96f44d9744d915a2e40fdc254491
Jason Li <jasonli1337@gmail.com>
2022-03-01 15:32:38 -0500

    Add Object Pool to Tree generator
    Create TextOfTree Prefab 
    Generator uses Coroutines to generate trees

--- Assets/TextTreeGenerator.cs
+++ Assets/TextTreeGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 namespace OvenFresh
@@ -13,12 +14,46 @@ namespace OvenFresh
         public float TreeDistanceLimit = 10f;
         
 
-        private List<GameObject> _AllTrees;
-        private Stack<GameObject> _TreePool;
+        private List<GameObject> _AllTrees = new List<GameObject>();
+        private Stack<GameObject> _TreePool = new Stack<GameObject>();
         
         void Start()
         {
             _AllTrees = new List<GameObject>();
+            for (var i = 0; i < 10; i++)
+            {
+                _AllTrees.Add(NewTree(transform.position + Vector3.right * i, Quaternion.identity, transform));
+            }
+
+            StartCoroutine(DestroyAllTreesIn(3));
+        }
+
+        IEnumerator DestroyAllTreesIn(float destroyInSeconds)
+        {
+            var elapsedTime = 0f;
+            while (elapsedTime < destroyInSeconds)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            foreach (GameObject tree in _AllTrees)
+            {
+                tree.GetComponent<TreeOfText>().Remove();
+            }
+
+            _AllTrees.Clear();
+            StartCoroutine(CreateTrees(Random.Range(3,25)));
+        }
+
+        IEnumerator CreateTrees(int numberOfTrees)
+        {
+            for (var i = 0; i < numberOfTrees; i++)
+            {
+                _AllTrees.Add(NewTree(transform.position + Vector3.right * i, Quaternion.identity, transform));
+            }
+            StartCoroutine(DestroyAllTreesIn(3));
+            yield return null;
         }
 
         void Update()
@@ -26,9 +61,30 @@ namespace OvenFresh
             
         }
 
+        public void AddToPool(GameObject tree)
+        {
+            _TreePool.Push(tree);
+        }
+        
         private GameObject NewTree(Vector3 position, Quaternion orientation, Transform parent)
         {
+            GameObject newTree;
+            if (_TreePool.Count > 0)
+            {
+                newTree = _TreePool.Pop();
+                newTree.SetActive(true);
+            }
+            else
+            {
+                newTree = Instantiate(TreeObject);
+                newTree.GetComponent<TreeOfText>().Init(this);
+            }
             
+            newTree.transform.position = position;
+            newTree.transform.localRotation = orientation;
+            newTree.transform.parent = parent;
+            
+            return newTree;
         }
     }
 }
---
+++ Assets/TreeOfText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OvenFresh
+{
+    public class TreeOfText : MonoBehaviour
+    {
+        public TextTreeGenerator Parent;
+
+        public void Init(TextTreeGenerator generator)
+        {
+            Parent = generator;
+        }
+        public void Remove()
+        {
+            Parent.AddToPool(gameObject);
+            gameObject.SetActive(false);
+        }
+        
+    }
+}
